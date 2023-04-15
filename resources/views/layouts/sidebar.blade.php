9b7c6574843caada0d2f663a1d6d700632ea9e91
Gauthier <gauthierdewit@gmail.com>
2023-04-15 23:03:12 +0200

    chore : redesign template for nav-link from sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,17 +2,27 @@
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
        <ul class="space-y-2 font-medium">
           <li>
-             <a href="{{ route('dashboard') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                <i class="fas fa-tachometer-alt text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
-                <span class="ml-3">Dashboard</span>
-             </a>
+            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" :fas="'tachometer-alt'">
+                {{ __('Dashboard') }}
+            </x-nav-link>            
           </li>
           <li>
-             <a href="{{ route('promotions.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                <i class="fas fa-building text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
-                <span class="flex-1 ml-3 whitespace-nowrap">Promotions</span>
-             </a>
+            <x-nav-link :href="route('promotions.index')" :active="request()->routeIs('promotions.*')" :fas="'building'">
+                {{ __('Promotions') }}
+            </x-nav-link>  
           </li>
        </ul>
-    </div>
+       <div id="dropdown-cta" class="p-4 mt-6 rounded-lg bg-blue-50 dark:bg-blue-900" role="alert">
+        <div class="flex items-center mb-3">
+           <span class="bg-orange-100 text-orange-800 text-sm font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-orange-200 dark:text-orange-900">Beta</span>
+           <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-900 rounded-lg focus:ring-2 focus:ring-blue-400 p-1 hover:bg-blue-200 inline-flex h-6 w-6 dark:bg-blue-900 dark:text-blue-400 dark:hover:bg-blue-800" data-dismiss-target="#dropdown-cta" aria-label="Close">
+                 <span class="sr-only">Close</span>
+                 <svg aria-hidden="true" class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+           </button>
+        </div>
+        <p class="mb-3 text-sm text-blue-800 dark:text-blue-400">
+           Preview the new management.app for rankings and promotions.
+        </p>
+     </div>
+    </div>  
  </aside>
