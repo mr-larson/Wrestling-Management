5c54266c96b57ba7fb7df8e8b4af52b77f9eb6a9
Gauthier <gauthierdewit@gmail.com>
2023-04-19 00:12:37 +0200

    chore : refactoring a component blade

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,9 +1,8 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Profile Information') }}
-        </h2>
-
+        <x-h2>
+            {{ __('Update Profile Information') }}
+        </x-h2>
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             {{ __("Update your account's profile information and email address.") }}
         </p>
