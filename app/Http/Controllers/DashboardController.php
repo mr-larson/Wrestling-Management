69ec692fc32263ef3dbeb3cc6eda054163fa6f9d
Gauthier <gauthierdewit@gmail.com>
2023-07-30 22:55:58 +0200

    fix : chrtJs in dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,40 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Promotion;
-use App\Models\Worker;
 use Illuminate\Http\Request;
-use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request): View
+    public function index()
     {
-        // Récupérer tous les workers avec leurs promotions
-        $workers = Worker::with('promotion')->get();
+        $promotions = Promotion::withCount('workers')->get();
 
-        // Créer un tableau pour stocker le nombre de workers par promotion
-        $promotionCounts = [];
+        $promotionNames = $promotions->pluck('name');
+        $workerCounts = $promotions->pluck('workers_count');
 
-        // Compter le nombre de workers par promotion
-        foreach ($workers as $worker) {
-            $promotion = $worker->promotion;
-            if ($promotion) {
-                if (!isset($promotionCounts[$promotion->name])) {
-                    $promotionCounts[$promotion->name] = 0;
-                }
-                $promotionCounts[$promotion->name]++;
-            }
-        }
-
-        // Trier les promotions par ordre décroissant en fonction du nombre de workers
-        arsort($promotionCounts);
-
-        // Extraire les labels et les valeurs pour le graphique
-        $promotionLabels = array_keys($promotionCounts);
-        $workerCounts = array_values($promotionCounts);
-
-        // Renvoyer la vue du tableau de bord en passant les données nécessaires
-        return view('dashboard', compact('promotionLabels', 'workerCounts'));
+        return view('dashboard', compact('promotionNames', 'workerCounts'));
     }
 }
-
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,34 +1,61 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+   <x-slot name="header">
+       <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+           {{ __('Dashboard') }}
+       </h2>
+   </x-slot>
 
-    <div class="p-4 sm:ml-64">
-        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+   <div class="p-4 sm:ml-64">
+       <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
            <div class="grid grid-cols-3 gap-4 mb-4">
                <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                  <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                   <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
                </div>
                <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                  <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                   <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
                </div>
                <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                  <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                   <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
                </div>
            </div>
            <div class="flex items-center justify-center h-52 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+               <canvas id="chart" style="height:400px; width:100%;"></canvas>
            </div>
            <div class="grid grid-cols-2 gap-4 mb-4">
-              <div class="flex items-center justify-center rounded bg-gray-50 h-48 dark:bg-gray-800">
-                 <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
-              </div>
-              <div class="flex items-center justify-center rounded bg-gray-50 h-48 dark:bg-gray-800">
-                 <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
-              </div>
+               <div class="flex items-center justify-center rounded bg-gray-50 h-48 dark:bg-gray-800">
+                   <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+               </div>
+               <div class="flex items-center justify-center rounded bg-gray-50 h-48 dark:bg-gray-800">
+                   <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+               </div>
            </div>
-        </div>
-     </div>
+       </div>
+   </div>
+
+   
+   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+   <script>
+      var ctx = document.getElementById('chart').getContext('2d');
+      new Chart(ctx, {
+         type: 'polarArea',
+         data: {
+               labels: {!! json_encode($promotionNames) !!},
+               datasets: [{
+                  label: 'Nombre de Workers',
+                  data: {!! json_encode($workerCounts) !!},
+                  backgroundColor: 'rgba(0, 123, 255, 0.5)',
+                  borderColor: 'rgba(0, 123, 255, 1)',
+                  borderWidth: 1
+               }]
+         },
+         options: {
+               scales: {
+                  y: {
+                     beginAtZero: true
+                  }
+               }
+         }
+      });
+   </script>
+
 </x-app-layout>
